e63f300eb257f9a7055ae6bcc2ab6cdc310f2cdd
arulchera <108506590+arulchera@users.noreply.github.com>
2024-04-30 18:14:36 +0530

    Update eks.tf after debugged

--- eks.tf
+++ eks.tf
@@ -1,27 +1,29 @@
-module "eks_eks-managed-node-group" {
-  source  = "terraform-aws-modules/eks/aws//modules/eks-managed-node-group"
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
   version = "20.8.5"
-  name            = "separate-eks-mng"
   cluster_name    = "my-cluster-eks"
   cluster_version = "1.27"
+  cluster_endpoint_public_access  = true
   subnet_ids = module.vpc.private_subnets
- 
-  vpc_id            = module.vpc.my_vpc.id
+  vpc_id            = module.vpc.vpc_id
 
-
-  min_size     = 2
-  max_size     = 4
-  desired_size = 2
+eks_managed_node_group_defaults = {
 
   instance_types = ["t3.large"]
   ami_type               = "AL2_x86_64"
   #capacity_type  = "SPOT"
-
-  labels = {
-    Environment = "dev"
-  
+}
+  eks_managed_node_groups = {
+    node_group = {
+  min_size     = 2
+  max_size     = 4
+  desired_size = 2
+    }
   }
-
+  tags = {
+    Environment = "dev"
+  }
+}
 
 
 
