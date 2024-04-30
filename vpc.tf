0f74779bb3bf96864272e125ec4335832ad022d5
arulchera <108506590+arulchera@users.noreply.github.com>
2024-04-30 18:13:25 +0530

    Update vpc.tf after debugged

--- vpc.tf
+++ vpc.tf
@@ -6,7 +6,7 @@ module "vpc" {
   name = "my-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = data.aws_availability_zones.avail_zone
+  azs             = data.aws_availability_zones.avail_zone.names
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets  = ["10.0.5.0/24", "10.0.6.0/24"]
 
@@ -15,10 +15,11 @@ module "vpc" {
   tags = {
     Environment = "dev"
   }
- }
+
 public_subnet_tags={
     Environment = "dev"
 }
 private_subnet_tags={
     Environment = "dev"
+}
 }
