ba12e622fc933fee38ffe01cfdcb916e2d4611f2
arulchera <108506590+arulchera@users.noreply.github.com>
2024-04-30 18:14:00 +0530

    Update security.tfafter debugged

--- security.tf
+++ security.tf
@@ -1,7 +1,7 @@
 resource "aws_security_group" "allow_all" {
   name        = "allow_tls"
   description = "Allow TLS inbound traffic and all outbound traffic"
-  vpc_id      = module.vpc.aws_vpc.id
+  vpc_id      = module.vpc.vpc_id
 
   tags = {
     Name = "allow_all"
@@ -10,11 +10,10 @@ resource "aws_security_group" "allow_all" {
 
 resource "aws_vpc_security_group_ingress_rule" "allow_all_ingress" {
   security_group_id = aws_security_group.allow_all.id
-  cidr_ipv4         = module.aws_vpc.my_vpc.cidr_block
+  cidr_ipv4         = "10.0.0.0/16"
   from_port         = 0
   ip_protocol       = "-1"
   to_port           = 0
-  cidr_blocks       = ["0.0.0.0/0"]
 
 }
 
@@ -22,7 +21,7 @@ resource "aws_vpc_security_group_ingress_rule" "allow_all_ingress" {
 resource "aws_vpc_security_group_egress_rule" "allow_all_egress" {
   security_group_id = aws_security_group.allow_all.id
   ip_protocol       = "-1" # semantically equivalent to all ports
-  cidr_blocks       = ["0.0.0.0/0"]
   from_port = 0
   to_port = 0
+  cidr_ipv4         = "0.0.0.0/0"
 }
